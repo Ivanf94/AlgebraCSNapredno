93f57963591fcc8c0f9f0396d99f6a1461970c99
Ivan Filipović <ifilip01@fesb.hr>
2025-03-30 17:48:54 +0200

    Modul 3 - predavanje 2

---
+++ Lambda_izrazi_i_uvjeti/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_izrazi_i_uvjeti
+{
+    internal class Program
+    {
+        delegate int MojDelegat(int p1, int p2, int p3);
+        static void Main(string[] args)
+        {
+            MojDelegat del = (x, y, z) => (x > y) ? (x > z ? x : z) : (y > z ? y : z);
+            Console.WriteLine(del(50,60,30));
+        }
+    }
+}
---
+++ Lambda_izrazi_primjer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_izrazi_primjer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> brojevi = new List<int>() { 36, 71, 12, 15, 29, 18, 27, 17, 9, 34 };
+
+            Console.WriteLine("Lista brojeva: ");
+            foreach(int broj in brojevi)
+            {
+                Console.Write($"{broj}  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var kvadrat_brojeva = brojevi.Select(x => x * x);
+            foreach(int broj in kvadrat_brojeva)
+            {
+                Console.Write($"{broj}  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            List<int> djeljivi_s_3 = brojevi.FindAll(x => (x % 3) == 0);
+            Console.WriteLine("Brojevi liste koji su djeljivi s 3: ");
+            foreach(int broj in djeljivi_s_3)
+            {
+                Console.Write($"{broj}  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
