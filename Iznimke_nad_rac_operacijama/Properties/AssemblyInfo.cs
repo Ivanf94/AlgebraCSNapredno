8bbe884fc9d4e6e36f5479ebd5706872f8349a68
Ivan Filipović <ifilip01@fesb.hr>
2025-04-01 12:08:53 +0200

    Modul 3 - predavanje 3

---
+++ Iznimke_nad_rac_operacijama/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Iznimke_nad_rac_operacijama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Iznimke_nad_rac_operacijama")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b35de86-3570-4ea2-89bf-a4039dca4fba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Iznimke_ugnjezdjivanje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iznimke_ugnjezdjivanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Molimo unesite broj nula: ");
+            try
+            {
+                int broj = int.Parse(Console.ReadLine());
+                try
+                {
+                    Console.WriteLine(100/broj);
+                }
+                catch
+                {
+                    Console.WriteLine("Iznimka unutar iznimke... exceptioninseption");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Obična iznimka...");
+            }
+        }
+    }
+}
