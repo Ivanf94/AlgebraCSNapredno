3041be88a63581ada81d7b82504bbcf703ee3341
Ivan Filipović <ifilip01@fesb.hr>
2025-04-06 19:41:32 +0200

    Modul 3 - predavanje 4

---
+++ Serijalizacija_dodatni_primjer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Serijalizacija_dodatni_primjer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KorisnickiDetalji kd = new KorisnickiDetalji(1, "Ana", "Anić", "Osijek");
+
+            Console.WriteLine("Prije serijalizacije objekt sadrži sljedeći podatke:");
+            kd.DohvatiDetalje();
+            Console.WriteLine();
+
+            string datoteka = "primjer.txt";
+
+            if (File.Exists(datoteka))
+            {
+                File.Delete(datoteka);
+            }
+
+            Stream pohrani = File.Open(datoteka, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(pohrani, kd);
+            pohrani.Close();
+
+            Console.WriteLine();
+            Console.WriteLine("Serijalizacija je uspješno odrađena...");
+
+            Stream citaj = File.Open(datoteka, FileMode.Open);
+            bf = new BinaryFormatter();
+            KorisnickiDetalji novi_detalji = (KorisnickiDetalji)bf.Deserialize(citaj);
+            citaj.Close();
+
+            Console.WriteLine();
+            Console.WriteLine("Nakon deserijalizacije podaci: ");
+            novi_detalji.DohvatiDetalje();
+        }
+    }
+}
---
+++ Serijalizacija_primjer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serijalizacija_primjer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Serijalizacija_primjer")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09e3405b-4a03-4451-997b-4e7a894324f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
